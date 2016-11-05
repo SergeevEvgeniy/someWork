024c6bc7eadbaa578a4235f6708e1a7e84075806
Yauheni Siarheyeu <sea2512@yandex.ru>
2016-11-05 13:23:12 +0300

    task14

--- tc/src/main/java/com/epam/tc/security/WebSecurityConfig.java
+++ tc/src/main/java/com/epam/tc/security/WebSecurityConfig.java
@@ -30,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/courses/create").hasAuthority("Lector")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
